51e318b90607f4b0c4d4d55071932b2482988f1f
peiyt <peiyutao156@163.com>
2020-06-09 16:27:16 +0800

    '测试'

--- src/main/java/com/jjgszl/springdatajpa/controller/RestControllerTest.java
+++ src/main/java/com/jjgszl/springdatajpa/controller/RestControllerTest.java
@@ -1,43 +1,43 @@
-package com.jjgszl.springdatajpa.controller;
-
-import com.jjgszl.springdatajpa.entity.Users;
-import com.jjgszl.springdatajpa.repository.UserInfoRepository;
-import org.omg.CORBA.INTERNAL;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-public class RestControllerTest {
-
-    @Autowired
-    UserInfoRepository repository;
-
-
-    @GetMapping("/test")
-    public Users queryUserByNameUseJPQL(Integer id){
-        if(id==null||id==0){
-            return null;
-        }
-        return repository.queryUserByNameUseJPQL(id);
-    }
-
-    @GetMapping("/test1")
-    public List<Users> queryUserByNameUseSQL(String userName){
-        if(userName==null||userName==""){
-            return null;
-        }
-        return repository.queryUserByNameUseSQL("wxw","175118");
-    }
-    @GetMapping("/test2")
-    public List<Users> queryUserByLikeNameUseSQL(){
-        return repository.queryUserByLikeNameUseSQL("%wx%");
-    }
-
-    @GetMapping("/test3")
-    public void  updateUserAgeById(){
-         repository.updateUserAgeById(175117,1);
-    }
-}
+//package com.jjgszl.springdatajpa.controller;
+//
+//import com.jjgszl.springdatajpa.entity.Users;
+////import com.jjgszl.springdatajpa.repository.UserInfoRepository;
+//import org.omg.CORBA.INTERNAL;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//public class RestControllerTest {
+////
+////    @Autowired
+////    UserInfoRepository repository;
+//
+//
+//    @GetMapping("/test")
+//    public Users queryUserByNameUseJPQL(Integer id){
+//        if(id==null||id==0){
+//            return null;
+//        }
+//        return repository.queryUserByNameUseJPQL(id);
+//    }
+//
+//    @GetMapping("/test1")
+//    public List<Users> queryUserByNameUseSQL(String userName){
+//        if(userName==null||userName==""){
+//            return null;
+//        }
+//        return repository.queryUserByNameUseSQL("wxw","175118");
+//    }
+//    @GetMapping("/test2")
+//    public List<Users> queryUserByLikeNameUseSQL(){
+//        return repository.queryUserByLikeNameUseSQL("%wx%");
+//    }
+//
+//    @GetMapping("/test3")
+//    public void  updateUserAgeById(){
+//         repository.updateUserAgeById(175117,1);
+//    }
+//}
--- src/main/java/com/jjgszl/springdatajpa/repository/UserInfoRepository.java
+++ src/main/java/com/jjgszl/springdatajpa/repository/UserInfoRepository.java
@@ -1,32 +1,32 @@
-package com.jjgszl.springdatajpa.repository;
-
-
-import com.jjgszl.springdatajpa.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
-import java.util.List;
-
-@Repository
-public interface UserInfoRepository extends JpaRepository<Users, Integer> {
-    //使用@Query注解查询
-    @Query(value = "from Users where userId =:userid")
-    Users queryUserByNameUseJPQL(Integer userid);
-
-    @Query(value="select * from t_users where username = ? and userage=?",nativeQuery=true)
-    List<Users> queryUserByNameUseSQL(String name,String userAge);
-
-    @Query(value="select * from t_users where username like ?",nativeQuery=true)
-    List<Users> queryUserByLikeNameUseSQL(String name);
-
-    @Query("update Users set userAge = ?1 where userId = ?2")
-    @Transactional
-    @Modifying  //@Modifying当前语句是一个更新语句
-    void updateUserAgeById(Integer age ,Integer id);
-
-
-}
+//package com.jjgszl.springdatajpa.repository;
+//
+//
+//import com.jjgszl.springdatajpa.entity.Users;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Modifying;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.stereotype.Repository;
+//
+//import javax.transaction.Transactional;
+//import java.util.List;
+//
+//@Repository
+//public interface UserInfoRepository extends JpaRepository<Users, Integer> {
+//    //使用@Query注解查询
+//    @Query(value = "from Users where userId =:userid")
+//    Users queryUserByNameUseJPQL(Integer userid);
+//
+//    @Query(value="select * from t_users where username = ? and userage=?",nativeQuery=true)
+//    List<Users> queryUserByNameUseSQL(String name,String userAge);
+//
+//    @Query(value="select * from t_users where username like ?",nativeQuery=true)
+//    List<Users> queryUserByLikeNameUseSQL(String name);
+//
+//    @Query("update Users set userAge = ?1 where userId = ?2")
+//    @Transactional
+//    @Modifying  //@Modifying当前语句是一个更新语句
+//    void updateUserAgeById(Integer age ,Integer id);
+//
+//
+//}
