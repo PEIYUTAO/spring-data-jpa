36d0d78fa9fa479cb4d6ef279457449b2fba6512
peiyt <peiyutao156@163.com>
2020-06-09 10:36:41 +0800

    sdfs

--- src/main/java/com/jjgszl/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/jjgszl/springdatajpa/SpringDataJpaApplication.java
@@ -9,6 +9,11 @@ import java.util.concurrent.Executors;
 public class SpringDataJpaApplication {
 
     public static void main(String[] args) {
+
+
+
+
+
         SpringApplication.run(SpringDataJpaApplication.class, args);
     }
 
