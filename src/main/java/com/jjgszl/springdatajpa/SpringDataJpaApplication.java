5dc3879813b575b60061b58d36317bd72e49fb3f
peiyt <peiyutao156@163.com>
2020-06-09 10:38:09 +0800

    '测试'

--- src/main/java/com/jjgszl/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/jjgszl/springdatajpa/SpringDataJpaApplication.java
@@ -11,9 +11,7 @@ public class SpringDataJpaApplication {
     public static void main(String[] args) {
 
 
-
-
-
+        System.out.println("测试提交代码到github");
         SpringApplication.run(SpringDataJpaApplication.class, args);
     }
 
