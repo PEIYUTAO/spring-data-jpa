3a024c8e888793c431b245049a0febd2066ee186
peiyt <peiyutao156@163.com>
2020-06-09 18:32:17 +0800

    'sss'

--- src/main/java/com/jjgszl/springdatajpa/controller/TestController.java
+++ src/main/java/com/jjgszl/springdatajpa/controller/TestController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String queryUserByNameUseJPQL(Integer id){
-
-
         System.out.println("webhook test");
         return "return success";
     }
