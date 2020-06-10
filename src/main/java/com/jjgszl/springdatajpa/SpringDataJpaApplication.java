285734efacd0ef00d1277ccce035cbe4bf03e43b
peiyt <peiyutao156@163.com>
2020-06-10 11:32:12 +0800

    ''

--- src/main/java/com/jjgszl/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/jjgszl/springdatajpa/SpringDataJpaApplication.java
@@ -10,7 +10,7 @@ public class SpringDataJpaApplication {
 
     public static void main(String[] args) {
 
-
+        System.out.println("last commit");
         SpringApplication.run(SpringDataJpaApplication.class, args);
     }
 
