941170b8305fd4cb5081fe34d0bf9064d92976ab
peiyt <peiyutao156@163.com>
2020-06-10 11:35:55 +0800

    '测试'

--- src/main/java/com/jjgszl/springdatajpa/controller/TestController.java
+++ src/main/java/com/jjgszl/springdatajpa/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
     @GetMapping("/test")
     public String queryUserByNameUseJPQL(Integer id){
         System.out.println("webhook test");
-        return "return success";
+        return "last commit";
     }
 }
